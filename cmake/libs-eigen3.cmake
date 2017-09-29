8881f8f065986b8c1aa14e49539a81b074826efb
itichi <itichi@gmail.com>
2017-09-29 13:46:59 +0200

    Added Eigen as a subfolder instead of find package

--- cmake/libs-eigen3.cmake
+++ cmake/libs-eigen3.cmake
@@ -7,7 +7,7 @@ message(STATUS "################################################")
 message(STATUS "Checking for Eigen3")
 
 # Find
-find_package(Eigen3 3.0.0 REQUIRED)
+find_package(Eigen3 REQUIRED)
 if (EIGEN3_FOUND)
     include_directories(SYSTEM ${EIGEN3_INCLUDE_DIR})
     add_definitions(-DEIGEN_USE_NEW_STDVECTOR -DEIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET)
